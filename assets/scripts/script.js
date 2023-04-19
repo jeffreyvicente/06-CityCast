6b0ebd67057882dee5f9366c094a1e37061b3040
Jeffrey Vicente <administrator@Jeffreys-MacBook-Air.local>
2023-04-19 10:48:31 -0700

    Made changes to HTML. Added event listener

---
+++ assets/scripts/script.js
@@ -0,0 +1,12 @@
+var searchButton = document.querySelector("#search-button");
+
+
+
+
+
+
+
+searchButton.addEventListener("click", function(){
+
+    console.log("Search button has been clicked!");
+})
